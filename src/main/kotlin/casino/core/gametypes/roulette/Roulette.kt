7ebf0808ee32dd30a67e556b827e21fc14fb2760
JaroslawKlepadlo <jarek.klepadlo@gmail.com>
2026-01-05 00:49:41 +0100

    Created Roulette game logic

--- src/main/kotlin/casino/core/gametypes/roulette/Roulette.kt
+++ src/main/kotlin/casino/core/gametypes/roulette/Roulette.kt
@@ -1,18 +1,16 @@
 package main.kotlin.casino.core.gametypes.roulette
 
-import main.kotlin.casino.core.game.GameType
-import main.kotlin.casino.core.game.IGame
-import main.kotlin.casino.core.game.IRoundBasedGame
 
 class Roulette private constructor(private val tiles: List<Tile>) {
-    var lastResult: Tile? = null
+    var lastResult: Tile = Tile(0, Color.GREEN)
+
     companion object {
         fun standardEuropean(): Roulette {
             val tiles = mutableListOf<Tile>()
             tiles.add(Tile(0, Color.GREEN))
             val redNumbers = setOf(
-                1,3,5,7,9,12,14,16,18,
-                19,21,23,25,27,30,32,34,36
+                1, 3, 5, 7, 9, 12, 14, 16, 18,
+                19, 21, 23, 25, 27, 30, 32, 34, 36
             )
             for (number in 1..36) {
                 val color = if (redNumbers.contains(number))
@@ -27,6 +25,7 @@ class Roulette private constructor(private val tiles: List<Tile>) {
     }
 
     fun spin(): Tile {
-        return TODO("Provide the return value")
+        lastResult = tiles.random()
+        return lastResult
     }
 }
